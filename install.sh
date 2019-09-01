303b36e21324848be4093adcb8e78d05c4074edd
micheljordan2323 <micheljordan2323@gmail.com>
2019-09-01 11:29:52 +0900

    add setupfile

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+sudp apt-get dist-upgrade
+
+
+#日本語化
+sudo apt-get install ttf-kochi-gothic xfonts-intl-japanese xfonts-intl-japanese-big xfonts-kaname
+sudo apt-get install jfbterm
+
+#lsusb
+sudo apt-get install fswebcam
+
+
+#PIGPIO
+sudo apt install pigpio
+
+
+#python関係
+sudo pip install --upgrade pip
+sudo apt-get install python3-pandas
+sudo apt-get install python-pandas
+
+#python libインストール
+pip install configparser
+pip install transitions
+
